b7980cab881f8d7cdfe5ad2636f496178b811fb2
alexnguyen2201 <n.vanson2021@gmail.com>
2022-07-14 08:55:23 +0700

    first commit

---
+++ tutorial1.py
@@ -0,0 +1,20 @@
+import boto3
+
+
+# client = boto3.client('s3')
+
+
+# response = client.list_buckets()
+
+# print(response)
+
+aws_resource = boto3.resource('s3')
+bucket = aws_resource.Bucket('nvson5')
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'ap-southeast-1'
+    },
+)
+
+print(response)
